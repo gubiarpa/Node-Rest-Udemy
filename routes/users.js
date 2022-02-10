2bef650e450cf5efb0c0a4df6f8b650858144590
Billy Arredondo <billy.arredondo@pucp.pe>
2022-02-10 11:58:12 -0500

    Centralizing DB validations

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const { usersGet, usersPut, usersPost, usersDelete, usersPatch } = require('../controllers/users');
+
 const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validate-fields');
-const Role = require('../models/role');
+const { isValidRole } = require('../helpers/db-validators');
 
 const router = Router();
 
@@ -13,12 +14,7 @@ router.put('/:id', usersPut);
 router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('password', 'Password must be a string with 6 characthers').isLength({min: 6}),
-    check('role').custom(async(role = '') => {
-        const roleExists = await Role.findOne({ role });
-        if (!roleExists) {
-            throw new Error(`The role ${ role } doesn't exist`);
-        }
-    }),
+    check('role').custom(isValidRole),
     check('email', 'Invalid email').isEmail(),
     validateFields,
     ],

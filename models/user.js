90f34213546389a14b063a913fb092c3b7731c50
Billy Arredondo <billy.arredondo@pucp.pe>
2022-02-10 11:15:05 -0500

    Adding validation against database

--- models/user.js
+++ models/user.js
@@ -20,7 +20,6 @@ const UserSchema = Schema({
     role: {
         type: String,
         required: true,
-        enum: ['ADMIN_ROLE', 'USER_ROLE']
     },
     state: {
         type: Boolean,
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { usersGet, usersPut, usersPost, usersDelete, usersPatch } = require('../controllers/users');
 const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validate-fields');
+const Role = require('../models/role');
 
 const router = Router();
 
@@ -12,7 +13,12 @@ router.put('/:id', usersPut);
 router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('password', 'Password must be a string with 6 characthers').isLength({min: 6}),
-    check('role', 'It is not a valid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('role').custom(async(role = '') => {
+        const roleExists = await Role.findOne({ role });
+        if (!roleExists) {
+            throw new Error(`The role ${ role } doesn't exist`);
+        }
+    }),
     check('email', 'Invalid email').isEmail(),
     validateFields,
     ],
